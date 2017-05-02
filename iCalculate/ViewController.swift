04b87ed7867d59013a3283555359a1b6acec443f
aluno <aluno@iMac-de-suporte-9.local>
2017-05-02 19:46:14 -0300

    Adding project files

--- iCalculate/ViewController.swift
+++ iCalculate/ViewController.swift
@@ -2,14 +2,16 @@
 //  ViewController.swift
 //  iCalculate
 //
-//  Created by aluno on 02/05/17.
-//  Copyright © 2017 aluno. All rights reserved.
+//  Created by Alysson Façanha Moreira on 02/05/17.
+//  Copyright © 2017 Alysson Façanha Moreira. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func btClear(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
